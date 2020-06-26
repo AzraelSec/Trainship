516be6c7a2df1fee16ce6df8671aa4f4d50907c6
AzraelSec <federicogerardi94@gmail.com>
2020-06-27 00:03:47 +0200

    Update MNIST Classification Test.py

--- MNIST Classification Test.py
+++ MNIST Classification Test.py
@@ -16,6 +16,9 @@ mnist = tf.keras.datasets.mnist
 
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
+x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
+x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
+
 model = tf.keras.models.Sequential([
     InputLayer(),
     Conv2D(64, (3, 3), activation='relu'),
