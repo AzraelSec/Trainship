b0b1d20e802d2a8f230c2926c423b1a3cfafa0c6
AzraelSec <federicogerardi94@gmail.com>
2020-07-20 22:10:05 +0200

    tested with touch command instead of copy

--- DatasetPreprocessor/preprocessor.sh
+++ DatasetPreprocessor/preprocessor.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 if [ "$#" -ne 2 ]; then
-    echo "usage: $0 [dataset_path] [number_of_families]"
+    echo "usage: $0 [dataset_path] [output_path]"
     exit 1
 fi
 
-if [ ! -d $1 ]; then
-    echo "error: $1 is not a valid directory"
+if [ ! -d $1 ] || [ ! -d $2 ]; then
+    echo "error: $1 or $2 is not a valid directory"
     exit 1
 fi
 
+############################
+##### SCRIPT SETTINGS ######
+############################
 FILES_PER_FAMILY=300
+FAMILIES_REQUIRED=8
 
 echo -e "\n######## ANALYSIS ########"
 collected_family_info=()
 for family in $(ls $1); do
-    ordered_complete=`find $1$family -type f -printf "%p %s\n" | sort -nk 2`
+    ordered_complete=`find $1/$family -type f -printf "%p %s\n" | sort -nk 2`
     complete_name=`echo "$ordered_complete" | cut -d ' ' -f 1`
     total_number=`echo "$complete_name" | wc -l`
     if [ "$total_number" -ge $FILES_PER_FAMILY ]; then
@@ -29,7 +33,7 @@ done
 echo -e "\n######## SORTED FAMILIES ########"
 sorted_selected_info=($(for info in ${collected_family_info[*]};do
     echo "$info" | tr '-' ' '
-done | sort -nrk 2 | head -n $2 | tr ' ' '-'))
+done | sort -nrk 2 | head -n $FAMILIES_REQUIRED | tr ' ' '-'))
 printf '%s\n' "${sorted_selected_info[@]}"
 
 sorted_selected_families=()
@@ -39,11 +43,15 @@ done
 
 echo -e "\n######## RESULTS ########"
 min_size=`echo "${collected_family_info[*]}" | tr ' ' '\n' | tr '-' ' ' | sort -nrk 2 | head -n 1 | cut -d ' ' -f 2`
-echo -e "\nYou need a minumum size of ${min_size} bytes from the fist ${2} families from above"
+echo -e "\nYou need a minumum size of ${min_size} bytes from the fist ${FAMILIES_REQUIRED} families"
 
 echo -e "\n######## RANDOM FILES CREATING ########"
 for family in ${sorted_selected_families[*]};do
-    selected_files_list=`find $1$family -type f -size -${min_size} -printf "%p\n" | head -n $FILES_PER_FAMILY | sort -R`
+    mkdir $2/$family
+    selected_files_list=`find $1/$family -type f -size -${min_size} -printf "%f\n" | head -n $FILES_PER_FAMILY | sort -R`
     selected_files_num=`echo "$selected_files_list" | wc -l`
+    for file in ${selected_files_list[*]};do
+        touch $2/$family/$file
+    done
     echo "I would have copied ${selected_files_num} files from $family"
 done
